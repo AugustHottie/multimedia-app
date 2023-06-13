509a260d00f85c4047133c6e0a2fb91e670e23ca
AugustHottie <chimexjessica@outlook.com>
2023-06-13 07:40:14 +0200

    mutlimedia file manager push

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/Introduction.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Labrinth-Express-Yourself.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/todo-github-readme.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/chioma.png"
+    },
+    {
+        id: 5,
+        name: 'Image File 2',
+        type: 'image',
+        path: "/file-server/blog_coverpage.png"
+      },
+      {
+        id: 6,
+        name: 'Image File 3',
+        type: 'image',
+        path: "/file-server/violets.png"
+      },
+  ]
